66045bf70a231facdc2a3936fed969d05533fa02
ArunaMoorthi <arunamoorthicbe@gmail.com>
2016-04-23 16:16:20 +0530

    Create largest_num.c

---
+++ largest_num.c
@@ -0,0 +1,15 @@
+//Program to check the greatest of three numbers
+#include <stdio.h>
+int main()
+{
+float a, b, c;
+printf("Enter three numbers: ");
+scanf("%f %f %f", &a, &b, &c);
+ if(a>=b && a>=c)
+   printf("Largest number = %.2f", a);
+    if(b>=a && b>=c)
+    printf("Largest number = %.2f", b);
+      if(c>=a && c>=b)
+         printf("Largest number = %.2f", c);
+      return 0;
+}
