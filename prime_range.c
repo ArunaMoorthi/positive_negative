fb2bfb8602a718ee278443760f5ea1fde85e717d
ArunaMoorthi <arunamoorthicbe@gmail.com>
2016-07-08 22:39:44 +0530

    Create prime_range.c

---
+++ prime_range.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int i, prime,n1,n2, n;
+clrscr();
+printf("\nEnter the range:");
+scanf("%d %d", &n1,&n2);
+printf("\nPRIME NUMBERS ARE…: ");
+for(n=n1+1; n<n1; n++)
+{
+prime = 1;
+for(i=2; i<n; i++)
+if(n%i == 0)
+{
+prime = 0;
+break;
+}
+if(prime)
+printf(“\n%d”, n);
+}
+getch();
+}
